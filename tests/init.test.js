3744ebbf022b8850847883384bd9228ecb9903d4
Eason Chai <echai2905@gmail.com>
2021-09-02 22:46:27 +0800

    Add linting config

--- tests/init.test.js
+++ tests/init.test.js
@@ -1,31 +1,31 @@
-const plugin = require("../lib/index");
+const plugin = require('../lib/index')
 
 const pluginConfig = (serviceAccount) => {
   return {
     upload: {
-      provider: "firebase-storage",
+      provider: 'firebase-storage',
       providerOptions: {
         serviceAccount,
         bucketUrl: process.env.STORAGE_BUCKET_URL,
         uploadOptions: {},
-        deleteOptions: {},
-      },
-    },
-  };
-};
+        deleteOptions: {}
+      }
+    }
+  }
+}
 
-test("Fail from incorrect service account", () => {
-  const config = pluginConfig("wrong_format");
-
-  expect(() => {
-    plugin.init(config);
-  }).toThrow();
-});
-
-test("Successfully init module", () => {
-  const config = pluginConfig(JSON.parse(process.env.SERVICE_ACCOUNT_JSON));
+test('Fail from incorrect service account', () => {
+  const config = pluginConfig('wrong_format')
 
   expect(() => {
-    plugin.init(config);
-  }).toThrow();
-});
+    plugin.init(config)
+  }).toThrow()
+})
+
+test('Successfully init module', () => {
+  const config = pluginConfig(JSON.parse(process.env.SERVICE_ACCOUNT_JSON))
+
+  expect(() => {
+    plugin.init(config)
+  }).toThrow()
+})

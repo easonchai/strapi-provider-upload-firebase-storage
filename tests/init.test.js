b3b25c716d0f9811c4df9d9fc6c6388d1e40c3dd
Eason Chai <echai2905@gmail.com>
2021-09-02 22:40:51 +0800

    Add tests for proper initialization

--- tests/init.test.js
+++ tests/init.test.js
@@ -1,19 +1,31 @@
 const plugin = require("../lib/index");
 
-test("Fail from incorrect service account", () => {
-  const pluginConfig = {
+const pluginConfig = (serviceAccount) => {
+  return {
     upload: {
       provider: "firebase-storage",
       providerOptions: {
-        serviceAccount: "wrong_format",
+        serviceAccount,
         bucketUrl: process.env.STORAGE_BUCKET_URL,
         uploadOptions: {},
         deleteOptions: {},
       },
     },
   };
+};
+
+test("Fail from incorrect service account", () => {
+  const config = pluginConfig("wrong_format");
 
   expect(() => {
-    plugin.init(pluginConfig);
+    plugin.init(config);
+  }).toThrow();
+});
+
+test("Successfully init module", () => {
+  const config = pluginConfig(JSON.parse(process.env.SERVICE_ACCOUNT_JSON));
+
+  expect(() => {
+    plugin.init(config);
   }).toThrow();
 });
